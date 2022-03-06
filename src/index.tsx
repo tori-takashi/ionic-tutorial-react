5f5aaecca5f79da6be1c99579cb7d7725ecc0939
Tori Takashi <tori.toutoshi@gmail.com>
2022-03-06 16:56:47 +0900

    final

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+defineCustomElements(window);
 
 ReactDOM.render(
   <React.StrictMode>
